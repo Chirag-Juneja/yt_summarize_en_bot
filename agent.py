e4f986ce96af5f89df8177fe7019a0177c34e6f1
Chirag Juneja <chiragjuneja6@gmail.com>
2025-02-10 17:40:28 +0530

    check valid url

--- agent.py
+++ agent.py
@@ -4,12 +4,14 @@ from langchain.prompts import PromptTemplate
 import os
 from dotenv import load_dotenv
 import argparse
-
+import logging
+logger = logging.getLogger(__name__)
 
 class YoutubeSummaryAgent:
 
-    def __init__(self):
-        self.model = OllamaLLM(model=os.getenv("MODEL"), temprature=0)
+    def __init__(self, model_name="llama3.2:1b"):
+        self.model = OllamaLLM(model=model_name, temprature=0)
+        logger.info(f"{model_name} model is loaded")
         self.template = """
         As an expert copywriter, your job is to create a point wise summary of the following transcript in simple to understand language.
         Your response should be only the summary.
@@ -19,20 +21,34 @@ class YoutubeSummaryAgent:
         self.chain = self.prompt | self.model
 
     def summarize(self, url):
-        loader = YoutubeLoader.from_youtube_url(url)
-        documents = loader.load()
-        response = self.chain.invoke({"transcript": documents[0].page_content})
+        self.is_valid_youtube_url(url)
+        if self.is_valid_youtube_url(url):
+            loader = YoutubeLoader.from_youtube_url(url)
+            documents = loader.load()
+            response = self.chain.invoke({"transcript": documents[0].page_content})
+        else:
+            response = "Invalid Youtube URL"
         return response
 
+    def is_valid_youtube_url(self, url):
+        import re
+        youtube_regex = re.compile(r'(https?://)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)/.+')
+        return youtube_regex.match(url)
 
 if __name__ == "__main__":
-    load_dotenv()
 
+    logging.basicConfig(level=logging.INFO)
+
+    load_dotenv()
+    
     parser = argparse.ArgumentParser(description="Youtube Summarizer")
     parser.add_argument("--url", type=str, help="youtube url")
     args = parser.parse_args()
 
-    agent = YoutubeSummaryAgent()
+    assert args.url is not None, "You must provide a YouTube URL."
+
+    model_name = os.getenv("MODEL")
+    agent = YoutubeSummaryAgent(model_name=model_name)
     summary = agent.summarize(args.url)
 
     print(summary)
